076a89196ecafd59b9ca8dd10dc5f24617c37d43
u548742577 <kdmorale@pichincha.com>
2024-10-24 20:15:40 -0600

    Add required to book schema

--- Models/Book.cs
+++ Models/Book.cs
@@ -3,10 +3,10 @@
   public class Book
     {
       public int Id { get; set; }
-      public string? Name { get; set; }
-      public string? Author { get; set; }
+      public required string? Name { get; set; }
+      public required string? Author { get; set; }
       public DateTime? PublicationDate { get; set; }
-      public string? Editorial { get; set; }
+      public required string? Editorial { get; set; }
       public string? ImagePath {get; set;}
       public DateTime CreatedAt { get; set; }
       public DateTime UpdatedAt { get; set; }
