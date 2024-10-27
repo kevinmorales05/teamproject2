ddbc8a0a9fb4a225785466ebd6042691bc2f44a7
samgo23 <gordsam18@gmail.com>
2024-10-26 20:30:44 -0600

    wishlist user features add

--- Migrations/20241027014952_AddWishListRelationships.Designer.cs
+++ Migrations/20241027014952_AddWishListRelationships.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Team2Project.Models;
 
@@ -10,9 +11,11 @@ using Team2Project.Models;
 namespace Team2Project.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241027014952_AddWishListRelationships")]
+    partial class AddWishListRelationships
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.10");
