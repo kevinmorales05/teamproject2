f5f2682be6f382e9902a68fad515e754d74da6c7
u548742577 <kdmorale@pichincha.com>
2024-10-28 17:26:32 -0600

    Implement add new book and getBooks from the database

--- Models/Book.cs
+++ Models/Book.cs
@@ -10,5 +10,7 @@ namespace Team2Project.Models
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
     public DateTime? DeletedAt { get; set; }
+    
   }
+  
 }
